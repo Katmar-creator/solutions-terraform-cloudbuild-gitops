e47993b82d9cac59bed67a0e3adfcbba56cb87a8
katmar-creator <katarzyna.s.marciniak@gmail.com>
2021-05-03 22:22:35 +0000

    prod version upgrade

--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -12,10 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 terraform {
   backend "gcs" {
     bucket = "katmar-21-tfstate"
     prefix = "env/prod"
   }
 }
+
