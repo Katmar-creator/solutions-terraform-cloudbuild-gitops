9b19acbad465aa35449d0559c4740c8f593de01c
katmar-creator <katarzyna.s.marciniak@gmail.com>
2021-05-03 22:11:45 +0000

    version upgrade

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -1,40 +1,47 @@
-# Copyright 2019 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+resource "google_bigtable_instance" "production-instance" {
+  name = "tf-instance"
+  deletion_protection = "false"
+  project = "${var.project}"
 
+  cluster {
+    cluster_id   = "tf-instance-cluster"
+    num_nodes    = 1
+    storage_type = "HDD"
+    zone         = "europe-west1-b"
+  }
 
-locals {
-  "env" = "prod"
+  labels = {
+    my-label = "prod-label"
+  }
 }
-
-provider "google" {
+resource "google_service_account" "default" {
+  account_id   = "service-account-id"
+  display_name = "Service Account"
   project = "${var.project}"
 }
 
-module "vpc" {
-  source  = "../../modules/vpc"
+resource "google_compute_instance" "default" {
+  name         = "test"
+  machine_type = "e2-medium"
+  zone         = "europe-west1-b"
   project = "${var.project}"
-  env     = "${local.env}"
-}
 
-module "http_server" {
-  source  = "../../modules/http_server"
-  project = "${var.project}"
-  subnet  = "${module.vpc.subnet}"
-}
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+  network_interface {
+    network = "default"
 
-module "firewall" {
-  source  = "../../modules/firewall"
-  project = "${var.project}"
-  subnet  = "${module.vpc.subnet}"
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    email  = "google-service-account-default@katmar-21.iam.gserviceaccount.com"
+    scopes = ["cloud-platform"]
+  }
 }
--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -12,11 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 output "network" {
-  value = "${module.vpc.network_name}"
+  value = module.vpc.network_name
 }
 
 output "subnet" {
-  value = "${element(module.vpc.subnets_names, 0)}"
+  value = element(module.vpc.subnets_names, 0)
 }
+
