6eaf6fac1532824a8310e6f6ef8451cd5e09bd86
katmar-creator <katarzyna.s.marciniak@gmail.com>
2021-05-04 17:55:29 +0000

    add vpc module to main

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -53,5 +53,12 @@ resource "google_compute_instance" "default" {
     email  = "google-service-account-default@katmar-21.iam.gserviceaccount.com"
     scopes = ["cloud-platform"]
   }
+
+  module "vpc" {
+  source  = "../../modules/vpc"
+  project = "${var.project}"
+  env     = "${local.env}"
+}
+
 }
 
